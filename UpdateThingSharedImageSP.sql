08a62604d7221ce8d670eb29587449c3bfb8688e
Marcus-Beach <52686467+Marcus-Beach@users.noreply.github.com>
2020-01-13 21:42:11 -0600

    20200113 Commit
    Fixed Transaction issues and added lots of updates.

---
+++ UpdateThingSharedImageSP.sql
@@ -0,0 +1,35 @@
+USE [PicturePlanner]
+GO
+/****** Object:  StoredProcedure [dbo].[CreateThingSP]    Script Date: 1/13/2020 8:18:24 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Marcus Beach
+-- Create date: 01/13/2020
+-- Description:	Update Thing Shared Image SP
+-- =============================================
+CREATE PROCEDURE [dbo].[UpdateThingSharedImageSP](@USERID AS INT, @THINGID AS INT, @SHAREDIMAGE AS NVARCHAR(50), @TRANSID AS INT = NULL)
+	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	BEGIN TRANSACTION
+    UPDATE Things
+	SET SharedImage = @SHAREDIMAGE
+	IF @TRANSID IS NULL
+	BEGIN
+		SET @TRANSID = ((SELECT top 1 TransactionID from Transactions order by TransactionID desc) + 1)
+		INSERT INTO Transactions(TransactionID, UserID, ThingID, Date)
+		VALUES (@TRANSID, @USERID, @THINGID, GETDATE())
+	END
+	ELSE
+	BEGIN
+		UPDATE Transactions SET UserID = @USERID, ThingID = @THINGID WHERE TransactionID = @TRANSID
+	END
+	COMMIT
+	SELECT @TRANSID
+END
